f3ea7648e864b66cc194b684fa634cffebcb2877
Mariya SOSNOVA <msosnova@paul-f3Ar2s2.clusters.42paris.fr>
2023-06-24 15:27:18 +0200

    ex06_3

--- Module_00/ex_06/recipe.py
+++ Module_00/ex_06/recipe.py
@@ -1,27 +1,57 @@
+import sys
+
+cookbook = {
+	'Sandwich': {'ingredients': ['ham', 'bread', 'cheese', 'tomatoes'],
+					'meal': 'lunch',
+					'prep_time': 10},
+	'Cake': {'ingredients': ['flour', 'sugar', 'eggs'],
+					'meal': 'dessert',
+					'prep_time': 60},
+	'Salad': {'ingredients': ['avocado', 'arugula', 'tomatoes', 'spinach'],
+					'meal': 'lunch',
+					'prep_time': 15}
+	}
+
 def print_dict():
+	global cookbook
 	for key in cookbook:
 		print(key)
 
 def print_recipe(recipe):
+	global cookbook
 	print(recipe, end = ': ')
-	for key in cookbook[recipe]['ingredients'] :
-		print(key, end=', ')
+	print(', '.join(cookbook[recipe]['ingredients']))
+
+def del_recipe(recipe):
+	global cookbook
+	cookbook.pop(recipe)
+
+
+
+def add_recipe():
+	global cookbook
+	recipe_name = input ("Enter a name:\n")
+	print("Enter ingredients:")
+	ingredients = sys.stdin.readlines()
+	meal = input ("Enter a meal type:\n")
+	prep_time = input ("Enter a preparation time:\n")
+	new_line = {recipe_name: {'ingredients': [f for f in ingredients],
+                				'meal': meal,
+                				'prep_time': prep_time}
+                }
+	cookbook.update(new_line)
+	print_dict()
+
+	
+	
 
 
 if __name__ == "__main__":
-	cookbook = {
-		'Sandwich': {'ingredients': ['ham', 'bread', 'cheese', 'tomatoes'],
-					 'meal': 'lunch',
-					 'prep_time': 10},
-		'Cake': {'ingredients': ['flour', 'sugar', 'eggs'],
-					 'meal': 'dessert',
-					 'prep_time': 60},
-		'Salad': {'ingredients': ['avocado', 'arugula', 'tomatoes', 'spinach'],
-					 'meal': 'lunch',
-					 'prep_time': 15}
-		}
 	print_dict()
-	print_recipe("Sandwich")
+	# print_recipe("Sandwich")
+	add_recipe()
+	del_recipe("chips")
 
+	print_dict()
 
 	
