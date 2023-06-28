10d75cd6228158f338727733f14eb1b7a50fd4e7
Sosnova <mariya@DESKTOP-OTTCFMH>
2023-06-28 08:54:46 +0200

    06

--- Module_00/ex_06/recipe.py
+++ Module_00/ex_06/recipe.py
@@ -30,17 +30,21 @@ def del_recipe(recipe):
 
 def add_recipe():
 	global cookbook
-	recipe_name = input ("Enter a name:\n")
+	# print(cookbook)
+	recipe_name = input("Enter a name:\n").capitalize()
 	print("Enter ingredients:")
-	ingredients = sys.stdin.readlines()
+	ingredients = []
+	while (ingredient := input()) != '':
+		ingredients.append(ingredient)
 	meal = input ("Enter a meal type:\n")
 	prep_time = input ("Enter a preparation time:\n")
-	new_line = {recipe_name: {'ingredients': [f for f in ingredients],
+	new_line = {recipe_name: {'ingredients': ingredients,
                 				'meal': meal,
                 				'prep_time': prep_time}
                 }
 	cookbook.update(new_line)
-	print_dict()
+	# print_dict()
+	print(cookbook)
 
 	
 	
@@ -48,9 +52,9 @@ def add_recipe():
 
 if __name__ == "__main__":
 	print_dict()
-	# print_recipe("Sandwich")
+	print_recipe("Sandwich")
 	add_recipe()
-	del_recipe("chips")
+	del_recipe("Chips")
 
 	print_dict()
 
